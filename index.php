d24cea64331108fc09396362cd83090f08a10e9d
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-11-14 12:51:43 +0100

    Index.php aangemaakt met SITE UNDER CONSTRUCTION

---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>SITE UNDER CONSTRUCTION</h1>
+<style>
+    h1 {
+        width: 100%;
+        text-align: center;
+        color: dodgerblue;
+        font-family: fantasy;
+        margin-top: 45vh;
+        font-size: 5em;
+    }
+</style>
