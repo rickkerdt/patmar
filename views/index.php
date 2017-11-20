3b7b6d7b2eaff5e2f608add7a493f8fd5271f63d
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 12:42:49 +0100

    V0.3 Content updatet

--- views/index.php
+++ views/index.php
@@ -20,11 +20,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <a class="navbar-left col" href="#">
-                <div class="d-none">
-                <img src="/resource/assets/logo.png" height="256" width="256" style="padding-top: 100px; position: absolute" class="img-fluid" alt="">
+                <div class="">
+                <img src="/resource/assets/logo.png" height="256" width="256" style="padding-top: 100px; position: absolute" class="d-lg-block" alt="">
                 </div>
                 <div class="d-sm-block d-md-block">
-                    <img src="/resource/assets/logo.png" height="64" width="64" style="padding-top: 100px" class="" alt="">
+                    <img src="/resource/assets/logo.png" height="64" width="64" style="padding-top: 100px" class="d-sm-block d-md-block" alt="">
                 </div>
             </a>
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
