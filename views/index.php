2394f26603692878b9e4dc18b6d5091dd3598ddc
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 11:55:40 +0100

    V0.3 Content updatet

--- views/index.php
+++ views/index.php
@@ -111,7 +111,7 @@
     </div>
 </div>
     <div class="col-md-9 col-sm-12">
-    <div class="card" style="padding-top: 50px;padding-left: 20px; border: 2px #00769f">
+    <div class="card wd-75" style="padding-top: 50px;padding-left: 20px; border: 2px #00769f">
         <div class="card-block">
             <H3 class="card-title">Over Patmar</H3>
         <p>Ons bedrijf Patmar is al meer dan 58 jaar een toonaangevend zonwering bedrijf in de regio Flevoland / Drenthe</p>
