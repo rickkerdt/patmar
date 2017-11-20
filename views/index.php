aba62393c63de19d0a664121642e11700c577ba8
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 12:04:58 +0100

    V0.3 Content updatet

--- views/index.php
+++ views/index.php
@@ -85,6 +85,61 @@
 
 </div>
 <div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <p class="card-block">
+                <p class="card-text">
+                <h3 class="card-title">Contact</h3>
+                <p class="card-text">
+                <h6 class="card-subtitle">A5 Patmar Marknesse</h6>
+                Hoge Sluiswal 47<br>
+                8316 AA Marknesse<br>
+                TEL: 0527 - 61 22 77<br>
+
+                <p>info@patmar.nl<br>
+                    www.patmar.nl</p>
+
+                <br><h6 class="card-subtitle">A5 Patmar Meppel</h6>
+                Johan van Oldenbarneveltstraat 3<br>
+                7942  GZ  MEPPEL<br>
+                Tel. 0522 - 24 48 88<br>
+
+                <p>info@patmar.nl<br>
+                    www.patmar.nl</p>
+            </p>
+                <a href="#" class="btn btn-primary">Route</a>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-block">
+                <h3 class="card-title">Over Patmar</h3>
+                <p class="card-text">
+                <p>Ons bedrijf Patmar is al meer dan 58 jaar een toonaangevend zonwering bedrijf in de regio Flevoland / Drenthe</p>
+
+                <p>Wij leveren aan particulieren en bedrijven, alle soorten zonwering, kunststof kozijnen, dakkapellen, garagedeuren, glasschuifwanden, terrasoverkappingen o.a. veranda's en tuinkamers. Ook raamdecoratie hebben wij in ons assortiment.</p>
+
+                <p>Als A5 Deco dealer onderscheiden wij ons door gedegen vakmanschap.<br>
+                    Al onze medewerkers zijn VCA opgeleid om optimaal advies te kunnen geven.<br>
+                    De werkzaamheden worden door ons eigen team uitgevoerd, al onze werknemers zijn gespecialiseerde vakmensen.</p>
+
+                <p>Zo dragen wij er zorg voor dat al de werkzaamheden correct en volgens afspraak worden uitgevoerd.<br>
+                    Wij als A5 dealer geven u graag een gedegen interieuradvies gericht op woningstoffering en kleurgebruik.<br>
+                    Daarnaast zult u zien dat de binnenzonweringen, buitenzonwering, kunststof kozijnen, garagedeuren, rolluiken ect wat kleur betreft goed op elkaar af te stemmen zijn.</p>
+
+                <p>Ook voor verandadaken ook wel veranda's genoemd bent u bij ons aan het juiste adres, er is een keuze uit verschillende goten rond, vlak en klassiek.<br>
+                    En er zijn verschillende soorten dakplaten veiligheidsglas, helder, opaal en de zonwerende plaat.</p>
+
+                <p>Sinds langere tijd zijn wij al dealer van VEKA Kunststof kozijnen deze kozijnen zijn een begrip in Nederland vanwege de goede kwaliteit en afwerking. Heeft u nieuwe kozijnen nodig bel of mail en wij zullen op al uw vragen een passend antwoord geven.</p>
+                </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!--<div class="row">
 <div class="col-md-2 d-none d-lg-block d-xl-block" style="padding-top: 50px;padding-left: 30px; border: 2px #00769f">
     <div class="card" style="width: 20rem; padding-left: 5px; border-color: #00769f">
         <div class="card-block">
@@ -133,7 +188,7 @@
         </div>
         </div>
 </div>
-</div>
+</div> -->
 </body>
 <footer>
 
