2d9783959bbf0b1baf677c2153d3993952058ccb
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 09:52:54 +0100

    V0.2 Navbar updated - Css added

--- views/index.php
+++ views/index.php
@@ -24,6 +24,7 @@
                     <span class="icon-bar"></span>
                 </button>
             </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
@@ -45,7 +46,6 @@
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-    </div>
 </header>
 <body>
 <div class="">
