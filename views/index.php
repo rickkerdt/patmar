1b9beffa512069de430516166220a262eb622e1c
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 09:56:44 +0100

    V0.2 Navbar updated - Css added

--- views/index.php
+++ views/index.php
@@ -17,7 +17,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <a class="navbar-brand visible-xs" href="index.php">Logo</a>
+                <a class="navbar-brand" href="index.php">Logo</a>
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
