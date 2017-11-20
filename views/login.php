fdd3303b0ee9802586fee53802f3bba243398fa5
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-11-20 12:19:58 +0100

    Simpele login formulier

--- views/login.php
+++ views/login.php
@@ -1,8 +1,36 @@
-<?php
-include_once "../classes/User.php";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Patmar - Login</title>
+</head>
+<body>
 
-echo "test";
+<div class="container">
+    <div class="row">
+        <div class="col">&nbsp;</div>
+        <div class="col-md-6">
+            <h1>Login</h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="form-control btn" value="Login">
+                </div>
+            </form>
+        </div>
+        <div class="col">&nbsp;</div>
+    </div>
+</div>
 
-//$user = new User();
-
-//$user->getUser(1);
+</body>
+</html>
