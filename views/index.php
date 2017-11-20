25cae4cba879729290dc83ebd3f12944536be317
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-20 11:01:56 +0100

    V0.2 Navbar updated - Css added

--- views/index.php
+++ views/index.php
@@ -64,7 +64,7 @@
         <p>Vindt ons op fbook<br>
             Bel ons op 06-00011122</p>
     </div> -->
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleSlidesOnly" class="carousel slide d-lg-block d-xl-block" data-ride="carousel">
         <div class="carousel-inner" role="listbox" data-interval="2000">
             <div class="carousel-item active">
                 <img class="d-block img-fluid" src="..\resource\slides\13.jpg" alt="First slide">
