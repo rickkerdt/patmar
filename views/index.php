cb0c9421f35085cc45f9a0a93b907d3775956504
JoppeOostenrijk <33652243+JoppeOostenrijk@users.noreply.github.com>
2017-11-21 13:37:46 +0100

    V0.5.1 Content updatet, navbar center - Moved login button to right side

--- views/index.php
+++ views/index.php
@@ -21,7 +21,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01" >
             <a class="navbar-left col" href="#">
-                <div class="d-none d-lg-block d-xl-block">
+                <div class=" d-none d-lg-block d-xl-block">
                 <img src="/resource/assets/logo.png" height="128" width="128" style="padding-top: 0px; position: absolute; border-radius: 35%" class="d-block" alt="1">
                 </div>
             </a>
@@ -47,6 +47,8 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Offerte Aanvraag</a>
                 </li>
+            </ul>
+            <ul class="navbar-nav ml-auto justify-content-end">
                 <li class="nav-item">
                     <a class="nav-link" href="patmar.win?page=login">Login</a>
                 </li>
