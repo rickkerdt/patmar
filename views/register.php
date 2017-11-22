e4126ad2082b375b5faa1349bd26ceec93c1119e
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-11-22 09:52:05 +0100

    register pagina bijgewerkt

--- views/register.php
+++ views/register.php
@@ -3,7 +3,7 @@
 if (isset($_POST["register"])) {
     $account = new Account();
 
-    if($_POST["password"] != $_POST["passwordrepeat"])
+    if ($_POST["password"] != $_POST["passwordrepeat"])
         die("incorrect password");
 
     try {
@@ -91,12 +91,21 @@ if (isset($_POST["register"])) {
                     <input type="email" class="form-control" name="email" id="email" placeholder="voorbeeld@mail.nl">
                 </div>
                 <div class="form-group">
-                    <label for="firstname">Voornaam</label>
-                    <input type="text" class="form-control" name="firstname" id="firstname" placeholder="Jan">
-                </div>
-                <div class="form-group">
-                    <label for="lastname">Achternaam</label>
-                    <input type="text" class="form-control" name="lastname" id="lastname" placeholder="de Vries">
+                    <div class="row">
+                        <div class="col-md-6">
+
+                            <label for="firstname">Voornaam</label>
+                            <input type="text" class="form-control" name="firstname" id="firstname" placeholder="Jan">
+                        </div>
+                        <div class="col-md-6">
+
+                            <label for="lastname">Achternaam</label>
+                            <input type="text" class="form-control" name="lastname" id="lastname"
+                                   placeholder="de Vries">
+                        </div>
+                    </div>
+                    <!--                </div>-->
+                    <!--                <div class="form-group">-->
                 </div>
                 <div class="form-group">
                     <label for="password">Wachtwoord</label>
