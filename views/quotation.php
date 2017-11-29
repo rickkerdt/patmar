fa105e3ab024f92cd07113eee13afdd4669b7b6c
Floris <floirsnijhof@gmail.com>
2017-11-29 10:40:42 +0100

    aanvraag formulier begin

--- views/quotation.php
+++ views/quotation.php
@@ -112,6 +112,13 @@
                         <div class="form-group">
                             <input type="submit" class="form-control btn btn-primary">
                         </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
