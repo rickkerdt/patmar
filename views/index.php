611dd6ab9b8a766ad415eb3a50a9434adfeb6352
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-06 09:17:34 +0100

    google maps toegevoegd.

--- views/index.php
+++ views/index.php
@@ -1021,3 +1021,6 @@
     </div>
     <script type="text/javascript">jssor_1_slider_init();</script>
 </div>
+<div class="map-responsive" style="width: 100%">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2417.519901554985!2d6.18677146522699!3d52.704760720686316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c87285002ce80b%3A0xf96a8753275e97c5!2sJohan+van+Oldenbarneveltstraat+3%2C+7942+GZ+Meppel!5e0!3m2!1snl!2snl!4v1481278702622" height="450" frameborder="0" style="border:0; width: inherit" allowfullscreen></iframe>
+</div>
