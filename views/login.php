1e5ac2ef919d7a933be430cd05141f16f5520625
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-13 10:14:23 +0100

    login login count

--- views/login.php
+++ views/login.php
@@ -5,40 +5,75 @@ if ($_SESSION["loggedIn"])
 $errors = [];
 
 if (isset($_POST["login"])) {
+    if (isset($_SESSION["wrongpassword"]) && $_SESSION["wrongpassword"] < 5) {
 
-    $url = "https://www.google.com/recaptcha/api/siteverify";
-    $data = [
-        "secret" => "6LdLDzwUAAAAAFjh7PJWnBXmxKTs87I03ZSCMwV8",
-        "response" => $_POST["g-recaptcha-response"],
-        "remoteip" => $_SERVER['REMOTE_ADDR']
-    ];
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $data = [
+            "secret" => "6LdLDzwUAAAAAFjh7PJWnBXmxKTs87I03ZSCMwV8",
+            "response" => $_POST["g-recaptcha-response"],
+            "remoteip" => $_SERVER['REMOTE_ADDR']
+        ];
 
-    $options = array(
-        'http' => array(
-            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method' => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
 
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
 
 
 //    if ($result["success"] == true) {
 //        if ($_POST["g-recaptcha-response"] != '') {
-            $user = new User();
-            if ($user->login($_POST["email"], $_POST["password"])) {
-                header("Location: /index.php?page=home");
+        $user = new User();
+        if ($user->login($_POST["email"], $_POST["password"])) {
+            header("Location: /index.php?page=home");
+        } else {
+            $errors = $user->errorList;
+            if (!isset($_SESSION["worngpassword"])) {
+                $_SESSION["wrongpassword"] = 1;
             } else {
-                $errors = $user->errorList;
+
+                $_SESSION["wrongpassword"] = $_SESSION["wrongpassword"] + 1;
             }
+        }
 //        } else {
 //            array_push($errors, "Los de recaptcha a.u.b. op.");
 //        }
 //    } else {
 //        array_push($errors, "Los de recaptcha a.u.b. op.");
 //    }
+    } else {
+        array_push($errors, "U heeft te vaak verkeerde wachtwoord ingevoerd.");
+
+        $time = $_SERVER['REQUEST_TIME'];
+
+        /**
+         * for a 30 minute timeout, specified in seconds
+         */
+        $timeout_duration = 1800;
+
+        /**
+         * Here we look for the user's LAST_ACTIVITY timestamp. If
+         * it's set and indicates our $timeout_duration has passed,
+         * blow away any previous $_SESSION data and start a new one.
+         */
+        if (isset($_SESSION['LAST_ACTIVITY']) &&
+            ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+            session_unset();
+            session_destroy();
+            session_start();
+        }
+
+        /**
+         * Finally, update LAST_ACTIVITY so that our timeout
+         * is based on it and not the user's login time.
+         */
+        $_SESSION['LAST_ACTIVITY'] = $time;
+    }
 }
 
 ?>
@@ -66,10 +101,10 @@ if (isset($_POST["login"])) {
                     <label for="password">Wachtwoord</label>
                     <input type="password" class="form-control" name="password" id="password" placeholder="********">
                 </div>
-<!--                <div class="form-group">-->
-<!--                    <script src="https://www.google.com/recaptcha/api.js" async defer></script>-->
-<!--                    <div class="g-recaptcha" data-sitekey="6LdLDzwUAAAAAO4DFj_pzlNNfrDeZ_up8UjwA_xj"></div>-->
-<!--                </div>-->
+                <!--                <div class="form-group">-->
+                <!--                    <script src="https://www.google.com/recaptcha/api.js" async defer></script>-->
+                <!--                    <div class="g-recaptcha" data-sitekey="6LdLDzwUAAAAAO4DFj_pzlNNfrDeZ_up8UjwA_xj"></div>-->
+                <!--                </div>-->
                 <div class="form-group">
                     <input type="submit" class="form-control btn btn-primary" name="login" value="Login">
                 </div>
