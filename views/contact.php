ba87d3fe97b40ed03268f2950eac76714ab4e86a
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-13 09:44:23 +0100

    recaptcha contact formulier test

--- views/contact.php
+++ views/contact.php
@@ -22,7 +22,7 @@ if (isset($_POST["contactsend"])) {
     $context = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
 
-    if ($result["success"] == true) {
+    if ($result["success"] == "true") {
         if ($contact->sendform($_POST["email"], $_POST["naam"], $_POST['adres'], $_POST['telefoonnumer'], $_POST['woonplaats'], $_POST['bericht'])) {
             $sent = true;
         } else {
