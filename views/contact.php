49088a848aa6140e6566504eb6cbf50eb9f4eeb9
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-13 09:27:12 +0100

    recaptcha contact formulier af

--- views/contact.php
+++ views/contact.php
@@ -1,14 +1,35 @@
 <?php
-if (isset($_POST["contactsend"])){
+$errors = [];
+if (isset($_POST["contactsend"])) {
     $contact = new Contact();
-    $errors = [];
     $sent = false;
 
-    if($contact->sendform($_POST["email"], $_POST["naam"],$_POST['adres'],$_POST['telefoonnumer'],$_POST['woonplaats'],$_POST['bericht']))
-    {
-        $sent = true;
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $data = [
+        "secret" => "6LdLDzwUAAAAAFjh7PJWnBXmxKTs87I03ZSCMwV8",
+        "response" => $_POST["g-recaptcha-response"],
+        "remoteip" => $_SERVER['REMOTE_ADDR']
+    ];
+
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result["success"] == true) {
+        if ($contact->sendform($_POST["email"], $_POST["naam"], $_POST['adres'], $_POST['telefoonnumer'], $_POST['woonplaats'], $_POST['bericht'])) {
+            $sent = true;
+        } else {
+            $errors = $contact->errorList;
+        }
     } else {
-        $errors = $contact->errorList;
+        array_push($errors, "Los de recaptcha a.u.b. op.");
     }
 }
 ?>
@@ -22,7 +43,7 @@ if (isset($_POST["contactsend"])){
 
             <br>
             <p>Heeft u vragen? &nbsp;<span style="line-height: 1.4;">Vul dan onderstaand formulier in.<br/>
-            Wij nemen dan zo spoedig mogelijk contact met u op.<br />
+            Wij nemen dan zo spoedig mogelijk contact met u op.<br/>
             Wilt u rechtstreeks contact dan kunt u ons bellen.</span></p>
             <br>
 
@@ -80,6 +101,10 @@ if (isset($_POST["contactsend"])){
                                       placeholder="Bericht *"></textarea>
                         </div>
                     </div>
+                    <div class="form-group">
+                        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                        <div class="g-recaptcha" data-sitekey="6LdLDzwUAAAAAO4DFj_pzlNNfrDeZ_up8UjwA_xj"></div>
+                    </div>
                     <div class="row">
                         <div class="form-group">
                             <input type="submit" value="Versturen" name="contactsend">
@@ -88,7 +113,7 @@ if (isset($_POST["contactsend"])){
                             <p>* Verplicht in te vullen</p>
                         </div>
                         <div class="form-group">
-                            <?php if($sent): ?>
+                            <?php if ($sent): ?>
                                 Het is verstuurd
                             <?php endif; ?>
                         </div>
@@ -112,7 +137,7 @@ if (isset($_POST["contactsend"])){
 
                     <br><h6 class="card-subtitle">A5 Patmar Meppel</h6>
                     Johan van Oldenbarneveltstraat 3<br>
-                    7942  GZ  MEPPEL<br>
+                    7942 GZ MEPPEL<br>
                     Tel. 0522 - 24 48 88<br>
 
                     <p>info@patmar.nl<br>
