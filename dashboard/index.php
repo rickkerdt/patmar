dd13b810b5ebbe9de279fa3dff5a72b721f94b4d
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-13 11:04:43 +0100

    dashboard redirect naar login als niet ingelogd is

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,7 +1,12 @@
 <?php
 
-function __autoload($class_name) {
+function __autoload($class_name)
+{
     include "../classes/" . $class_name . '.php';
+}
+
+if (!isset($_SESSION["loggedIn"])) {
+    header("Location: /index.php?page=login");
 }
 
 include_once "template.php";
