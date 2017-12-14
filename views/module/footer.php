a1cebd94b3330187bc7240371eb0c764bfdb707b
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-14 13:10:11 +0100

    offerte pagina + class + postcheck bijgewerkt

--- views/module/footer.php
+++ views/module/footer.php
@@ -1,20 +1,36 @@
-<footer style="background-color: #212529; min-height: 250px; margin-top: -10px;padding-top: 50px; ">
-
-    <div style="background-color: #1b1e21; height: 200px">
-        <div class="container" style="color: white; padding-top: 50px;">
-            <div class="row text-center">
-                <div class="col">
-                    <p>Patmar</p>
-                    <p>Patmar 2</p>
-                    <p>Patmar 3</p>
+<footer style="background-color: #212529;">
+    <div style="background-color: #1b1e21;">
+    <div class="container mx-auto" style="color: white; padding-top: 20px; font-size: 0.7em;">
+        <!-- begin bovenste rij footer -->
+        <div class="row">
+            <!-- begin bovenste kolom footer -->
+            <div class="d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block col-3 mx-auto">
+                <p>Patmar.nl<br>
+                    Home<br>
+                    Producten<br>
+                    Contactaanvraag<br>
+                    Storing melden<br>
+                    Offerteaanvraag
+                </p>
+            </div>
+            <div class="col-md-6 col-sm-12 mx-auto">
+                <p>
+                Contact info:<br>
+                    <div class="row">
+                    <div class="col-6 ">
+                    <strong>A5&nbspPatmar&nbspMarknesse</strong><br>
+                        <span>Hoge&nbspSluiswal&nbsp47,</span> <span>8316&nbspAA&nbspMarknesse.</span><br>
+                        <span>TEL:&nbsp0527&nbsp&#8209&nbsp61&nbsp22&nbsp77</span> || <span>info@patmar.nl</span>
                 </div>
-                <div class="col">
-                    <p>
-                        Ontworpen en gerealiseerd door: Coachng groep 5
-                    </p>
+                <div class="col-6">
+                    <strong>A5&nbspPatmar&nbspMeppel</strong><br>
+                    Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
+                    TEL:&nbsp0522&nbsp&#8209&nbsp24&nbsp48&nbsp88 || info@patmar.nl
                 </div>
+                </div>
+                </p>
             </div>
         </div>
     </div>
-
+    </div>
 </footer>
