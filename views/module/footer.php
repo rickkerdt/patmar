24497af3d1cd26426d9cfac247328df54d07fbb0
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-14 13:35:16 +0100

    offerte pagina + class + postcheck bijgewerkt

--- views/module/footer.php
+++ views/module/footer.php
@@ -19,13 +19,13 @@
                     <div class="row">
                     <div class="col-6 ">
                     <strong>A5&nbspPatmar&nbspMarknesse</strong><br>
-                        <span>Hoge&nbspSluiswal&nbsp47,</span> <span>8316&nbspAA&nbspMarknesse.</span><br>
-                        <span>TEL:&nbsp0527&nbsp&#8209&nbsp61&nbsp22&nbsp77</span> || <span>info@patmar.nl</span>
+                        <a href="https://www.google.nl/maps/place/Hoge+Sluiswal+47,+8316+AA+Marknesse/">Hoge&nbspSluiswal&nbsp47, 8316&nbspAA&nbspMarknesse.<br>
+                            TEL:&nbsp0527&nbsp&#8209&nbsp61&nbsp22&nbsp77 || info@patmar.nl</a>
                 </div>
                 <div class="col-6">
                     <strong>A5&nbspPatmar&nbspMeppel</strong><br>
-                    Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
-                    TEL:&nbsp0522&nbsp&#8209&nbsp24&nbsp48&nbsp88 || info@patmar.nl
+                    <a href="https://www.google.nl/maps/place/Johan+van+Oldenbarneveltstraat+3,+7942+GZ+Meppel/">Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
+                        TEL:&nbsp0522&nbsp&#8209&nbsp24&nbsp48&nbsp88 || info@patmar.nl</a>
                 </div>
                 </div>
                 </p>
