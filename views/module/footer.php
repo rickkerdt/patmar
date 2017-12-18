c24c29be479e269271597161c68b0d545e44d5fa
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-18 14:39:58 +0100

    footer link color

--- views/module/footer.php
+++ views/module/footer.php
@@ -5,12 +5,12 @@
         <div class="row">
             <!-- begin bovenste kolom footer -->
             <div class="d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block col-3 mx-auto">
-                <p><a href="https://patmar.win">Patmar.nl</a><br>
-                    <a href="https://patmar.win">Home</a><br>
-                    <a href="https://patmar.win#producten">Producten</a><br>
-                    <a href="https://patmar.win?page=contact">Contactaanvraag</a><br>
-                    <a href="https://patmar.win?page=storing">Storing melden</a><br>
-                    <a href="https://patmar.win?page=offerte">Offerteaanvraag</a>
+                <p><a href="https://patmar.win" class="footer">Patmar.nl</a><br>
+                    <a href="https://patmar.win" class="footer">Home</a><br>
+                    <a href="https://patmar.win#producten" class="footer">Producten</a><br>
+                    <a href="https://patmar.win?page=contact" class="footer">Contactaanvraag</a><br>
+                    <a href="https://patmar.win?page=storing" class="footer">Storing melden</a><br>
+                    <a href="https://patmar.win?page=offerte" class="footer">Offerteaanvraag</a>
                 </p>
             </div>
             <div class="col-md-6 col-sm-12 mx-auto">
@@ -19,12 +19,12 @@
                     <div class="row">
                     <div class="col-6 ">
                     <strong>A5&nbspPatmar&nbspMarknesse</strong><br>
-                        <a href="https://www.google.nl/maps/place/Hoge+Sluiswal+47,+8316+AA+Marknesse/">Hoge&nbspSluiswal&nbsp47, 8316&nbspAA&nbspMarknesse.<br>
+                        <a href="https://www.google.nl/maps/place/Hoge+Sluiswal+47,+8316+AA+Marknesse/" class="footer">Hoge&nbspSluiswal&nbsp47, 8316&nbspAA&nbspMarknesse.<br>
                             TEL:&nbsp0527&nbsp&#8209&nbsp61&nbsp22&nbsp77 || info@patmar.nl</a>
                 </div>
                 <div class="col-6">
                     <strong>A5&nbspPatmar&nbspMeppel</strong><br>
-                    <a href="https://www.google.nl/maps/place/Johan+van+Oldenbarneveltstraat+3,+7942+GZ+Meppel/">Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
+                    <a href="https://www.google.nl/maps/place/Johan+van+Oldenbarneveltstraat+3,+7942+GZ+Meppel/" class="footer">Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
                         TEL:&nbsp0522&nbsp&#8209&nbsp24&nbsp48&nbsp88 || info@patmar.nl</a>
                 </div>
                 </div>
