614600d87db96956e6124da6b346c160e38be092
Rickkerdt <rick.paassen@outlook.com>
2017-12-19 09:58:57 +0100

    agenda template reupload

---
+++ dashboard/add_events.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rickpaassen
+ * Date: 19-12-17
+ * Time: 09:55
+ */
+$title = $_POST['title'];
+$start = $_POST['start'];
+$end = $_POST['end'];
+
+
+try {
+    require "db_config.php";
+} catch(Exception $e) {
+    exit('Unable to connect to database.');
+}
+
+
+$sql = "INSERT INTO events (title, start, end) VALUES (:title, :start, :end )";
+$q = $bdd->prepare($sql);
+$q->execute(array(':title'=>$title, ':start'=>$start, ':end'=>$end));
+
+
