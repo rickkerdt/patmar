b0668488ddf6df50f054465576aaf83b39489815
Rickkerdt <rick.paassen@outlook.com>
2017-12-19 09:21:35 +0100

    agenda template reupload

--- dashboard/views/agenda.php
+++ dashboard/views/agenda.php
@@ -13,12 +13,6 @@
     <p class="lead">
         This agenda viewer will let you see multiple events cleanly!
     </p>
-
-    <div class="alert alert-warning">
-        <h4>Mobile Support</h4>
-        <p>In order to get the lines between cells looking their best without any JavaScript, I had to use tables for this design. While this could be done in ".row", doing so will cause issues when displaying the vertical borders between cells, which is a compromise I wasn't willing to make this time.'</p>
-    </div>
-
     <hr />
 
     <div class="agenda">
