a4524e9be7760e7695cd113c5e08e6a6127f05fb
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-19 11:00:12 +0100

    User class fix

--- classes/User.php
+++ classes/User.php
@@ -66,9 +66,10 @@ class User
 //      Verbinding maken met database
         $db = new PDO("mysql:host=localhost;dbname=patmar;", "patmar", "Patmar1!");
 //      Query opbouwen
-        $q2 = $db->prepare("INSERT INTO Userinfo( ?, FirstName, LastName) VALUES (?,?)");
+        $q2 = $db->prepare("INSERT INTO Userinfo( UserInfoID, FirstName, LastName, UserID) VALUES (?,?,?,?)");
 //      Anti SQL injectie
         $q2->bindValue(1, $userID);
+        $q2->bindValue(4, $userID);
         $q2->bindValue(2, $firstname);
         $q2->bindValue(3, $lastname);
 //      Query uitvoeren
