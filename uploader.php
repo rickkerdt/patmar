db6b2d7b7d0b27a194c654a05493e168565a0821
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-19 14:47:51 +0100

    uploader test

--- uploader.php
+++ uploader.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST["upload"]))
 {
-    $dir = "/uploads";
+    $dir = getcwd(). "/uploads";
     $file = $dir . "/" . basename($_FILES["file"]["name"]);
     try {
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $file)){
