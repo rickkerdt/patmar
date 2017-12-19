ff689b5c2a81d9836c84a3e3614a07189c6b3703
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-19 17:22:58 +0100

    full view entry contact

--- dashboard/views/contactfull.php
+++ dashboard/views/contactfull.php
@@ -65,8 +65,8 @@ $contact = $contact->getContact($_GET["Contactid"]);
                         <div class="col-md-9 text-truncate">
                             <div class="form-group">
 
-                            <input type="text" rows="6" class="form-control disabledInput" name="bericht" id="Bericht"
-                                      placeholder="<?php echo $contact["Bericht"]; ?>" readonly="readonly" style="background: white; color: black !important; font-weight: bolder;">
+                            <textarea rows="6" class="form-control" name="bericht" id="Bericht"
+                                      placeholder="<?php echo $contact["Bericht"]; ?>" readonly="readonly" style="background: white; color: black !important; font-weight: bolder;"></textarea>
 
                         </div>
                     </div>
