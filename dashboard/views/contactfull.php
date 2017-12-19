b4ca0df4a8e1f002d00bd4d184920272ef4f1a8e
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-19 14:12:33 +0100

    full view entry contact

--- dashboard/views/contactfull.php
+++ dashboard/views/contactfull.php
@@ -14,55 +14,55 @@ $contact = $contact->getContact($_GET["Contactid"]);
             <li class="list-group-item">
                 <form action="/dashboard/" method="get">
                     <div class="row">
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <strong>Contactnummer</strong>
                         </div>
                         <input type="hidden" name="userID" value="<?php echo $contact["Contactid"]; ?>">
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Contactid"]; ?>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Naam</strong>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Naam"]; ?>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Adres</strong>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Adres"]; ?>
                         </div>
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Woonplaats</strong>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Woonplaats"]; ?>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Email</strong>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Email"]; ?>
                         </div>
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Telefoonnummer</strong>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-9">
                             <?php echo $contact["Telefoonnummer"]; ?>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2 text-truncate">
+                        <div class="col-md-3 text-truncate">
                             <strong>Bericht</strong>
                         </div>
-                        <div class="col-md-8 text-truncate">
+                        <div class="col-md-9 text-truncate">
                             <?php echo $contact["Bericht"]; ?>
                         </div>
                     </div>
