21af8ec605cb98159f79a4976caf3f547ffbd165
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-19 14:56:30 +0100

    uploader test

--- uploader.php
+++ uploader.php
@@ -1,28 +1,30 @@
 <?php
+if(isset($_FILES['image'])){
+    $errors= array();
+    $file_name = $_FILES['image']['name'];
+    $file_size =$_FILES['image']['size'];
+    $file_tmp =$_FILES['image']['tmp_name'];
+    $file_type=$_FILES['image']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
 
-if(isset($_POST["upload"]))
-{
-    echo $_FILES["file"]["name"];
-    $dir = "uploads/";
-    $file = $dir . basename($_FILES["file"]["name"]);
-    echo $file;
-    try {
-        if(move_uploaded_file($_FILES["file"]["tmp_name"], $file)){
-            echo "works!";
-            die();
-        } else {
-            echo "doesn't work!";
-            die();
-        }
-    } catch (Exception $e)
-    {
-        var_dump($e);
-        die();
+    $expensions= array("jpeg","jpg","png");
+
+    if(in_array($file_ext,$expensions)=== false){
+        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+    }
+
+    if($file_size > 2097152){
+        $errors[]='File size must be excately 2 MB';
+    }
+
+    if(empty($errors)==true){
+        move_uploaded_file($file_tmp,"images/".$file_name);
+        echo "Success";
+    }else{
+        print_r($errors);
     }
 }
-
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -34,7 +36,7 @@ if(isset($_POST["upload"]))
 </head>
 <body>
 <form action="/uploader.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="file">
+    <input type="file" name="image">
     <input type="submit" name="upload" value="Upload">
 </form>
 </body>
