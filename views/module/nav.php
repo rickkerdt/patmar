cadb6a3d5b2204072c8ea0fa7a5b594c86dffc9e
Rickkerdt <rick.paassen@outlook.com>
2017-12-20 13:50:35 +0100

    dsahboard link fix

--- views/module/nav.php
+++ views/module/nav.php
@@ -48,7 +48,7 @@
                     </li>
                 <?php elseif ($_SESSION["loggedIn"] != ""): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="/dashboard/?page=dashboard">Dashboard</a>
+                        <a class="nav-link" href="/dashboard/?page=home">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="?page=logout">Logout</a>
