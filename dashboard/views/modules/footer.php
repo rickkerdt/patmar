5981a19fa2e6225cf70c2265107e32568f664686
Rickkerdt <rick.paassen@outlook.com>
2017-12-20 09:23:23 +0100

    Link in dashboard voor contact

--- dashboard/views/modules/footer.php
+++ dashboard/views/modules/footer.php
@@ -1,3 +1,36 @@
 <footer>
-
+    <div style="">
+        <div class="container mx-auto" style="color: white; padding-top: 20px; font-size: 0.7em;">
+            <!-- begin bovenste rij footer -->
+            <div class="row">
+                <!-- begin bovenste kolom footer -->
+                <div class="d-none d-xs-none d-sm-none d-md-block d-lg-block d-xl-block col-3 mx-auto footer">
+                    <p><a href="https://patmar.win" class="footer">Patmar.nl</a><br>
+                        <a href="https://patmar.win" class="footer">Home</a><br>
+                        <a href="https://patmar.win#producten" class="footer">Producten</a><br>
+                        <a href="https://patmar.win?page=contact" class="footer">Contactaanvraag</a><br>
+                        <a href="https://patmar.win?page=storing" class="footer">Storing melden</a><br>
+                        <a href="https://patmar.win?page=offerte" class="footer">Offerteaanvraag</a>
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mx-auto footer">
+                    <p>
+                        Contact info:<br>
+                    <div class="row">
+                        <div class="col-6 ">
+                            <strong>A5&nbspPatmar&nbspMarknesse</strong><br>
+                            <a href="https://www.google.nl/maps/place/Hoge+Sluiswal+47,+8316+AA+Marknesse/" class="footer">Hoge&nbspSluiswal&nbsp47, 8316&nbspAA&nbspMarknesse.<br>
+                                TEL:&nbsp0527&nbsp&#8209&nbsp61&nbsp22&nbsp77 || info@patmar.nl</a>
+                        </div>
+                        <div class="col-6">
+                            <strong>A5&nbspPatmar&nbspMeppel</strong><br>
+                            <a href="https://www.google.nl/maps/place/Johan+van+Oldenbarneveltstraat+3,+7942+GZ+Meppel/" class="footer">Johan&nbspvan Oldenbarneveltstraat&nbsp3, 7942&nbspGZ&nbspMEPPEL<br>
+                                TEL:&nbsp0522&nbsp&#8209&nbsp24&nbsp48&nbsp88 || info@patmar.nl</a>
+                        </div>
+                    </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
