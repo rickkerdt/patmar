eac6fb726b748f549703f9af8a2401b0223fac2b
Rickkerdt <rick.paassen@outlook.com>
2017-12-20 09:19:36 +0100

    Link in dashboard voor contact

--- dashboard/views/modules/nav.php
+++ dashboard/views/modules/nav.php
@@ -6,6 +6,9 @@
         </div>
         <ul class="navbar-nav mx-auto w-100 justify-content-end">
             <li class="nav-item active"><a class="nav-link" style="color: white !important;" href="#"><?php echo $_SESSION["email"] ?></a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="?page=logout">Logout</a>
+            </li>
         </ul>
     </nav>
 </header>
