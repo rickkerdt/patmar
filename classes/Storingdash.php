d1d633c7435b3075481719e7b445a9132a67f2e5
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-20 14:34:13 +0100

    full view entry offerte

--- classes/Storingdash.php
+++ classes/Storingdash.php
@@ -16,13 +16,13 @@ class Storingdash
         $this->db = new PDO("mysql:host=localhost;dbname=patmar;", "patmar", "Patmar1!");
     }
 
-    public function getStoringList($pagination = 0)
+    public function getStoringenList($pagination = 0)
     {
         $q = $this->db->prepare("SELECT * FROM Storing s JOIN Userinfo u ON s.UserID = u.UserID LIMIT 10 OFFSET ?");
         $q->bindValue(1, intval($pagination * 10, 10), \PDO::PARAM_INT);
 
         if ($q->execute()) {
-            return $q->fetchAll();
+            return $q->fetchAll()[0];
         } else {
             return $q->errorCode();
         }
