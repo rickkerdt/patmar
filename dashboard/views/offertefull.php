462b7cebf5fe8db11ab7c20b07a01b8ed8ca8bc1
Joppe Oostenrijk <s1119339@student.windesheim.nl>
2017-12-20 12:47:02 +0100

    full view entry offerte

---
+++ dashboard/views/offertefull.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joppe
+ * Date: 20-12-2017
+ * Time: 12:43
+ */
+
+$offerte = new Offertedash();
+$offerte = $offerte->getOfferte($_GET["OfferteID"]);
+$bericht = $offerte["Bericht"];
+?>
+<div class="row">
+    <div class="col-md-12">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <form action="/dashboard/" method="get">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <strong>Contactnummer</strong>
+                        </div>
+                        <input type="hidden" name="userID" value="<?php echo $offerte["OfferteID"]; ?>">
+                        <div class="col-md-9">
+                            <?php echo $offerte["OfferteID"]; ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-truncate">
+                            <strong>Naam</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo $offerte["Naam"]; ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-truncate">
+                            <strong>Adres</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo $offerte["Adres"]; ?>
+                        </div>
+                        <div class="col-md-3 text-truncate">
+                            <strong>Woonplaats</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo $offerte["Woonplaats"]; ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3 text-truncate">
+                            <strong>Email</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo $offerte["Email"]; ?>
+                        </div>
+                        <div class="col-md-3 text-truncate">
+                            <strong>Telefoonnummer</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo $offerte["Telefoonnummer"]; ?>
+                        </div>
+                    </div>
+                    <div class="row" style="height: 200px;">
+                        <div class="col-md-3">
+                            <strong>Bericht</strong>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="form-group">
+                                <?php print nl2br($bericht) ?>
+                            </div>
+                        </div>
+            </li>
+        </ul>
+    </div>
+</div>
