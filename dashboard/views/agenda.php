0652cf88e435aa8832d93b378818fd0bfa0ae534
Patiphan Mahamat <patiphan.mahamat@gmail.com>
2017-12-21 09:56:33 +0100

    agenda permissie fix

--- dashboard/views/agenda.php
+++ dashboard/views/agenda.php
@@ -1,121 +1,121 @@
-<?php if ($_SESSION["permission"] != "3" || $_SESSION["permission"] != "1") : ?>
-    <div class='alert alert-danger'><strong>Fout!</strong> Niet toegestaan!</div>
-<?php elseif ($_SESSION["permission"] == "3" || $_SESSION["permission"] == "1"): ?>
-
-<script>
-    $(document).ready(function () {
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
+<?php if ($_SESSION["permission"] == "3" || $_SESSION["permission"] == "1") : ?>
+    <script>
+        $(document).ready(function () {
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
 
 
-        var calendar = $('#calendar').fullCalendar({
-            editable: true,
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
+            var calendar = $('#calendar').fullCalendar({
+                editable: true,
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
 
 
-            events: "/dashboard/events.php",
+                events: "/dashboard/events.php",
 
 
-            eventRender: function (event, element, view) {
-                if (event.allDay === 'true') {
-                    event.allDay = true;
-                } else {
-                    event.allDay = false;
-                }
-            },
-            selectable: true,
-            selectHelper: true,
-            select: function (start, end, allDay) {
-                var title = prompt('Event Title:');
+                eventRender: function (event, element, view) {
+                    if (event.allDay === 'true') {
+                        event.allDay = true;
+                    } else {
+                        event.allDay = false;
+                    }
+                },
+                selectable: true,
+                selectHelper: true,
+                select: function (start, end, allDay) {
+                    var title = prompt('Event Title:');
 
 
-                if (title) {
-                    var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                    if (title) {
+                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                        $.ajax({
+                            url: '/dashboard/add_events.php',
+                            data: 'title=' + title + '&start=' + start + '&end=' + end,
+                            type: "POST",
+                            success: function (json) {
+                                alert('Added Successfully');
+                                console.log(json);
+                            }
+                        });
+                        calendar.fullCalendar('renderEvent',
+                            {
+                                title: title,
+                                start: start,
+                                end: end,
+                                allDay: allDay
+                            },
+                            true
+                        );
+                    }
+                    calendar.fullCalendar('unselect');
+                },
+
+
+                editable: true,
+                eventDrop: function (event, delta) {
+                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
                     $.ajax({
-                        url: '/dashboard/add_events.php',
-                        data: 'title=' + title + '&start=' + start + '&end=' + end,
+                        url: '/dashboard/update_events.php',
+                        data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
                         type: "POST",
                         success: function (json) {
-                            alert('Added Successfully');
+                            alert("Updated Successfully");
                             console.log(json);
                         }
                     });
-                    calendar.fullCalendar('renderEvent',
-                        {
-                            title: title,
-                            start: start,
-                            end: end,
-                            allDay: allDay
-                        },
-                        true
-                    );
-                }
-                calendar.fullCalendar('unselect');
-            },
-
-
-            editable: true,
-            eventDrop: function (event, delta) {
-                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                $.ajax({
-                    url: '/dashboard/update_events.php',
-                    data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
-                    type: "POST",
-                    success: function (json) {
-                        alert("Updated Successfully");
-                        console.log(json);
+                },
+                eventClick: function (event) {
+                    var decision = confirm("Do you really want to do that?");
+                    if (decision) {
+                        $.ajax({
+                            type: "POST",
+                            url: "/dashboard/delete_event.php",
+                            data: "&id=" + event.id,
+                            success: function (json) {
+                                $('#calendar').fullCalendar('removeEvents', event.id);
+                                alert("Updated Successfully");
+                                console.log(json);
+                            }
+                        });
                     }
-                });
-            },
-            eventClick: function (event) {
-                var decision = confirm("Do you really want to do that?");
-                if (decision) {
+                },
+                eventResize: function (event) {
+                    var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+                    var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
                     $.ajax({
+                        url: '/dashboard/update_events.php',
+                        data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
                         type: "POST",
-                        url: "/dashboard/delete_event.php",
-                        data: "&id=" + event.id,
                         success: function (json) {
-                            $('#calendar').fullCalendar('removeEvents', event.id);
                             alert("Updated Successfully");
                             console.log(json);
                         }
                     });
                 }
-            },
-            eventResize: function (event) {
-                var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
-                var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
-                $.ajax({
-                    url: '/dashboard/update_events.php',
-                    data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
-                    type: "POST",
-                    success: function (json) {
-                        alert("Updated Successfully");
-                        console.log(json);
-                    }
-                });
-            }
+
+            });
 
         });
 
-    });
 
+    </script>
+    <style>
+        #calendar {
+            width: inherit;
+            margin: 0 auto;
+        }
+    </style>
 
-</script>
-<style>
-    #calendar {
-        width: inherit;
-        margin: 0 auto;
-    }
-</style>
+    <div id='calendar'></div>
+<?php else : ?>
 
-<div id='calendar'></div>
+    <div class='alert alert-danger'><strong>Fout!</strong> Niet toegestaan!</div>
 <?php endif; ?>
